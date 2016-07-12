8d26acbc62a72f34c03a452740c3da7d260109d0
Fan Zhang <ffanzhang@hotmail.com>
2016-07-11 22:57:57 -0700

    heroku does not have required c++ library, using python 3 for now

--- routes/index.js
+++ routes/index.js
@@ -21,21 +21,20 @@ router.get('/ide', function(req, res, next) {
 router.post('/ide', function(req, res, next) {
   var source = req.body.code;
   var input = req.body.input;
-  console.log(source);
-  console.log(input);
   var out = "";
 
+  var touch = exec('touch dummy');
   //var hello = exec('echo \"' + source.toString() + '\" > dummy.cpp');
-  fs.writeFileSync('dummy.cpp', source);
+  fs.writeFileSync('dummy', source);
   fs.writeFileSync('input.txt', input);
-  var cat = exec('cat dummy.cpp');
+  var cat = exec('cat dummy');
   cat.stdout.on('data', function(data) {
     console.log(String(data));
   });
 
-
-  var gpp = spawn('g++', ['dummy.cpp']);
-
+  var python3 = exec('python3 dummy < input.txt');
+/*
+  var gpp = spawn('g++', ['dummy']);
   gpp.stdout.on('data', function(data) {
     res.send(String(data));
   });
@@ -46,7 +45,7 @@ router.post('/ide', function(req, res, next) {
 
   gpp.on('close', function(data) {
     if (data === 0) {
-      var run = exec('./a.out < input.txt');
+      var run = exec('./a.out < input');
       run.stdout.on('data', function(out) {
         res.send(String(out));
       });
@@ -60,8 +59,13 @@ router.post('/ide', function(req, res, next) {
       });
     }
   });
+*/
+  python3.stdout.on('data', function(out) {
+    console.log(String(out));
+    res.send(String(out));
+  });
 
-  var rm = exec('rm dummy.cpp');
+  var rm2 = exec('rm a.out');
 
 });
 

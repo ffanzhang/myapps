266654b312a4a5a117ff15ea28946a2e46ddbe7c
Fan Zhang <ffanzhang@hotmail.com>
2016-07-18 22:51:11 -0700

    changed location of redirection so that middleware can parse relevant info

--- app.js
+++ app.js
@@ -14,6 +14,13 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
 if (app.get('env') == 'production') {
   app.use(function(req, res, next) {
     if (!req.secure) {
@@ -24,12 +31,6 @@ if (app.get('env') == 'production') {
   });
 }
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'node_modules/codemirror')));
 

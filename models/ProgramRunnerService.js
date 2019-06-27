f4754e43ca89094614d8d29d310ddb6633607c97
Fan Zhang <ffanzhang@hotmail.com>
2019-06-26 23:58:33 -0700

    moved the rest of the dispatcher helpers to ProgramRunnerService

--- models/ProgramRunnerService.js
+++ models/ProgramRunnerService.js
@@ -1,6 +1,45 @@
 const ProgramRunner = require('./ProgramRunner');
+const path = require('path');
+const crypto = require('crypto');
+const fs = require('fs');
+const exec = require('child_process').exec;
+const execSync = require('child_process').execSync;
 
 class ProgramRunnerService {
+  static process_options() {
+    return {
+      cwd: '/tmp/',
+      env: {
+        PATH: process.env.PATH
+      }
+    };
+  };
+
+  static cleanup(foldername, filename, executablename) {
+    execSync('rm -rf ' + foldername, ProgramRunnerService.process_options());
+  };
+
+  static make_files(source, ext) {
+    let files = [];
+    let foldername = path.join("tmpsrc", crypto.randomBytes(16).toString('hex'));
+    let filename = path.join(foldername, crypto.randomBytes(32).toString('hex') + ext);
+    let executablename = path.join(foldername, crypto.randomBytes(32).toString('hex'));
+
+    if (!fs.existsSync(path.join('/tmp', 'tmpsrc'))) {
+      execSync('mkdir tmpsrc', ProgramRunnerService.process_options());
+    }
+
+    execSync('mkdir ' + foldername, ProgramRunnerService.process_options());
+    execSync('touch ' + filename, ProgramRunnerService.process_options());
+
+    fs.writeFileSync(path.join('/tmp', filename), source);
+
+    files.push(foldername);
+    files.push(filename);
+    files.push(executablename);
+    return files;
+  };
+
   static dispatch(req, res) {
     let source = req.body.code;
     let input = req.body.input;
@@ -16,41 +55,42 @@ class ProgramRunnerService {
           res.send(out);
         });
         break;
-      case "gcc":
-        files = make_files(source, '.c');
-        foldername = files[0];
-        filename = files[1];
-        executablename = files[2];
+      case "gcc": {
+        let files = ProgramRunnerService.make_files(source, '.c');
+        let foldername = files[0];
+        let filename = files[1];
+        let executablename = files[2];
         ProgramRunner.run('gcc', [filename, '-o', executablename], input, function(out, data) {
           if (data == 0) {
             ProgramRunner.run('./' + executablename, [], input, function(out, data) {
-              cleanup(foldername, filename, executablename);
+              ProgramRunnerService.cleanup(foldername, filename, executablename);
               res.send(out);
             });
           } else {
-            cleanup(foldername, filename, executablename);
+            ProgramRunnerService.cleanup(foldername, filename, executablename);
             res.send(out);
           }
         });
-
         break;
-      case "g++":
-        files = make_files(source, '.cc');
-        foldername = files[0];
-        filename = files[1];
-        executablename = files[2];
+      }
+      case "g++": {
+        let files = ProgramRunnerService.make_files(source, '.cc');
+        let foldername = files[0];
+        let filename = files[1];
+        let executablename = files[2];
         ProgramRunner.run('g++', [filename, '--std=c++11', '-o', executablename], input, function(out, data) {
           if (data == 0) {
             ProgramRunner.run('./' + executablename, [], input, function(out, data) {
-              cleanup(foldername, filename, executablename);
+              ProgramRunnerService.cleanup(foldername, filename, executablename);
               res.send(out);
             });
           } else {
-            cleanup(foldername, filename, executablename);
+            ProgramRunnerService.cleanup(foldername, filename, executablename);
             res.send(out);
           }
         });
         break;
+      }
       default:
         ProgramRunner.run('python', ['-c', source], input, function(out) {
           res.send(out);
--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,9 @@
 const express = require('express');
-const path = require('path');
 const router = express.Router();
 const request = require('request');
-const spawn = require('child_process').spawn;
-const exec = require('child_process').exec;
-const execSync = require('child_process').execSync;
-const fs = require('fs');
-const crypto = require('crypto');
-const Promise = require('bluebird');
 const octicons = require('octicons');
 const ProgramRunnerService = require('../models/ProgramRunnerService');
 
-const process_options = {
-  cwd : '/tmp/',
-  env : {
-    PATH: process.env.PATH
-  }
-};
-
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Fan\'s Apps' });
 });
@@ -34,31 +19,6 @@ router.get('/ide', function(req, res, next) {
 let handle_error = function(e) {
   console.log(e);
 }
-
-let cleanup = function(foldername, filename, executablename) {
-  execSync('rm -rf ' + foldername, process_options);
-}
-
-let make_files = function(source, ext) {
-  let files = [];
-  let foldername = path.join("tmpsrc", crypto.randomBytes(16).toString('hex'));
-  let filename = path.join(foldername, crypto.randomBytes(32).toString('hex') + ext);
-  let executablename = path.join(foldername, crypto.randomBytes(32).toString('hex'));
-
-  if (!fs.existsSync(path.join('/tmp', 'tmpsrc'))) {
-    execSync('mkdir tmpsrc', process_options);
-  }
-
-  execSync('mkdir ' + foldername, process_options);
-  execSync('touch ' + filename, process_options);
-
-  fs.writeFileSync(path.join('/tmp', filename), source);
-
-  files.push(foldername);
-  files.push(filename);
-  files.push(executablename);
-  return files;
-};
 
 router.post('/ide', function(req, res, next) {
     ProgramRunnerService.dispatch(req, res);
